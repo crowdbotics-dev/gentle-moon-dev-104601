b3511a4d2db94a50873672cc4cf10501e5e60774
Crowdbotics <engineering@crowdbotics.com>
2023-07-26 16:00:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-moon-dev-104601",
+  url: "https://gentle-moon-dev-104601.botics.co",
+  api: "https://gentle-moon-dev-104601.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
